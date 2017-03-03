6f344e2533b9813afa009a24e1b7259b3346df77
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2017-03-03 10:20:13 +0100

    asd

--- Datoruppgift/2a.py
+++ Datoruppgift/2a.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sigma = 2.8 * 10**(-23) # m^2
-tau = 230.0 # µs
-N_naught = 1.4 * 10**20 # cm^-3
-D = 8.0 # mm
-La = 20.0 # cm
+tau = 230.0 * 10**(-6) # µs
+N_naught = 1.4 * 10**20 * 10**(-6) # cm^-3 > m^-3
+D = 8.0 * 10**(-3) # mm
+La = 20.0 * 10**(-2) # cm
 c = 299792458.0 # m/s
 
 R1 = 1.1
@@ -23,28 +23,35 @@ V = La * math.pi * (D/2)**2 # La or L ??
 P = N_inf / tau
 B = (sigma * c) / V
 
-dt = 0.01
+dt = 0.1
 
-def N(T_i):
-    if (T_i == 0):
+V_a = math.pi * (4 * 10**(-3))**2 * 20 * 10**(-2)
+
+def T_i(i):
+    if (dt * i < 0):
         return 0
     else:
-        return N(T_i - 1) + (P - B * N(T_i - 1) * phi(T_i - 1) - N(T_i - 1) / tau) * dt
+        return dt * i
 
-def phi(T_i):
-    if (T_i == 0):
-        return 0
-    else:
-        return phi(T_i - 1) + ((B * V * N(T_i - 1))*(phi(T_i - 1) + 1) - phi(T_i - 1) / tau_c) * dt
+def N(x):
+    phi_ans = phi(T_i(x-1))
+    N_ans = N(T_i(x-1))
+    return N_ans + (P - B * N_ans * phi_ans - N_ans / tau) * dt
 
-time_i = range(0, 12)
+def phi(x):
+    phi_ans = phi(T_i(x-1))
+    N_ans = N(T_i(x-1))
+    return phi_ans + (((B * V_a * N_ans)*(phi_ans + 1) - phi_ans) / tau_c) * dt
 
-N_y_res = []
-phi_y_res = []
+#time = range(0, 1)
+#N_arr = []
+#phi_arr = []
 
-for i in time_i:
-    N_y_res.append(N(i))
-    phi_y_res.append(phi(i))
+#for i in time:
+    #N_arr.append(N(i))
+    #phi_arr.append(phi(i))
 
-plt.plot(time_i, N_y_res, 'r--', time_i, phi_y_res, 'b');
+print(N(1))
+
+plt.plot(time, N_arr, 'r--', time, phi_arr, 'b')
 plt.show()
