11e5111f753cca90119b7b856d493748c62090e8
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2017-03-03 12:26:23 +0100

    2a

--- Datoruppgift/2a.py
+++ Datoruppgift/2a.py
@@ -11,8 +11,8 @@ D = 8.0 * 10**(-3) # mm
 La = 20.0 * 10**(-2) # cm
 c = 299792458.0 # m/s
 
-R1 = 1.1
-R2 = 1.1
+R1 = 1
+R2 = 0.05
 
 tau_c = (-2.0 * La) / (c * (math.log(R1) + math.log(R2))) # La or L ??
 
@@ -23,7 +23,7 @@ V = La * math.pi * (D/2)**2 # La or L ??
 P = N_inf / tau
 B = (sigma * c) / V
 
-dt = 0.1
+dt = 200 * 10**(-6)
 
 V_a = math.pi * (4 * 10**(-3))**2 * 20 * 10**(-2)
 
@@ -34,24 +34,33 @@ def T_i(i):
         return dt * i
 
 def N(x):
+    if x <= 0:
+        return N_naught
     phi_ans = phi(T_i(x-1))
     N_ans = N(T_i(x-1))
     return N_ans + (P - B * N_ans * phi_ans - N_ans / tau) * dt
 
 def phi(x):
+    if x <= 0:
+        return 0
     phi_ans = phi(T_i(x-1))
     N_ans = N(T_i(x-1))
     return phi_ans + (((B * V_a * N_ans)*(phi_ans + 1) - phi_ans) / tau_c) * dt
 
-#time = range(0, 1)
-#N_arr = []
-#phi_arr = []
+time = range(0, 20)
+x_arr = []
+N_arr = []
+phi_arr = []
 
-#for i in time:
-    #N_arr.append(N(i))
-    #phi_arr.append(phi(i))
+for i in time:
+    x_arr.append(T_i(i))
+    N_arr.append(N(i))
+    phi_arr.append(phi(i))
 
-print(N(1))
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].plot(x_arr, phi_arr)
+axarr[0].set_title('PHI')
+axarr[1].plot(x_arr, N_arr)
+axarr[1].set_title('N')
 
-plt.plot(time, N_arr, 'r--', time, phi_arr, 'b')
 plt.show()
