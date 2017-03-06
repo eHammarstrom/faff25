753d06b435893e52a0738009d7cf22ae4056ea57
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2017-03-06 14:03:09 +0100

    3d render of assigment 1c

--- Datoruppgift/1c.py
+++ Datoruppgift/1c.py
@@ -3,6 +3,8 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
 
 a1 = 2.271176
 a2 = -9.700709 * 10**(-3)
@@ -16,6 +18,10 @@ h = 2.5
 
 lambda_wave = np.linspace(0.4, 0.7, 1000)
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
 def n(wavelen):
     return np.sqrt(a1 +
             a2*wavelen**2 +
@@ -24,9 +30,16 @@ def n(wavelen):
             a5*wavelen**(-6) +
             a6*wavelen**(-8))
 
+X, Y = np.meshgrid(lambda_wave, n(lambda_wave))
+
 alfa = np.arcsin(h/R) - np.arcsin(h/(R*n(lambda_wave)))
 
 f = h / np.sin(alfa)
 
-plt.plot(lambda_wave, f)
+Z = h / np.sin(alfa)
+
+# plt.plot(n(lambda_wave), f)
+
+ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, edgecolor=(0,0,0,0))
+
 plt.show()
